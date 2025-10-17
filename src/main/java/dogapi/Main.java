6bbea3c640d5ad0d491c1017408c419867fd5e97
Julia Znaczko <lanatkachuk@MacBookPro.localdomain>
2025-10-17 14:39:59 -0400

    update

--- src/main/java/dogapi/Main.java
+++ src/main/java/dogapi/Main.java
@@ -10,6 +10,7 @@ public class Main {
         int result = getNumberOfSubBreeds(breed, breedFetcher);
         System.out.println(breed + " has " + result + " sub breeds");
 
+        //should return the custom error
         breed = "cat";
         result = getNumberOfSubBreeds(breed, breedFetcher);
         System.out.println(breed + " has " + result + " sub breeds");
@@ -24,8 +25,12 @@ public class Main {
      * returned by the fetcher
      */
     public static int getNumberOfSubBreeds(String breed, BreedFetcher breedFetcher) {
-        // TODO Task 3 implement this code so that it is entirely consistent with its provided documentation.
-        // return statement included so that the starter code can compile and run.
-        return -1;
+
+        try {
+            List<String> subBreeds = breedFetcher.getSubBreeds(breed);
+            return subBreeds.size();
+        } catch (BreedFetcher.BreedNotFoundException e) {
+            return 0;
+        }
     }
 }
